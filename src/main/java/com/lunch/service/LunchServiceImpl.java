463e4437d5dc1c1d8a6a6a059f62265ce8c36cd6
bibekrajdhakal <bibek.dhakal@petcircle.com.au>
2020-10-12 20:17:54 +1100

    filter out recipes with ingredients past the use by date sort the recipes with ingredients past the best before date to be at the bottom

--- src/main/java/com/lunch/service/LunchServiceImpl.java
+++ src/main/java/com/lunch/service/LunchServiceImpl.java
@@ -7,6 +7,10 @@ import com.lunch.repository.RecipesRepository;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Inject;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,10 +28,33 @@ public class LunchServiceImpl implements LunchService {
     public RecipesList get() {
         List<Recipes> recipes = repository.findAll();
 
+        List<Recipes> recipesWithIngredientsWithInUseByDate = recipes.stream()
+                .filter(this::isWithInUseByDate)
+                .sorted(Comparator.comparing(this::isWithInBestBeforeDate).reversed())
+                .collect(Collectors.toList());
+
         RecipesList list = new RecipesList();
-        list.setRecipesResources(recipes.stream().map(RecipesResource::from).collect(Collectors.toList()));
+        list.setRecipesResources(recipesWithIngredientsWithInUseByDate.stream()
+                .map(RecipesResource::from)
+                .collect(Collectors.toList()));
 
         return list;
+    }
+
+    private boolean isWithInUseByDate(Recipes recipes) {
+        return recipes.getIngredients()
+                .stream()
+                .allMatch(ingredients -> isFirstDateBeforeSecondDate(LocalDate.now(), ingredients.getUseBy()));
+    }
+
+    private boolean isWithInBestBeforeDate(Recipes recipes) {
+        return recipes.getIngredients()
+                .stream()
+                .allMatch(ingredients -> isFirstDateBeforeSecondDate(LocalDate.now(), ingredients.getBestBefore()));
+    }
+
+    private boolean isFirstDateBeforeSecondDate(LocalDate firstDate, Date secondDate) {
+        return firstDate.isBefore(secondDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
     }
 
 }
